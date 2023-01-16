98e843478189306c269409d5e61b44a1a8c980e8
Olga Ioffe <yoffic@yahoo.com>
2023-01-17 02:42:52 +0400

    add task list view

---
+++ views/task-list/TaskList.tsx
@@ -0,0 +1,30 @@
+import { List } from '@mui/material';
+
+import { Tasks } from 'entities/task/model';
+import TaskCard from 'entities/task/ui/TaskCard';
+import TaskRow from 'entities/task/ui/TaskRow';
+import ToggleTask from 'features/toggle-task/ui';
+
+interface ITaskListProps {
+  tasks?: Tasks;
+}
+
+function TaskList(props: ITaskListProps) {
+  const { tasks } = props;
+
+  if (!tasks) return null;
+
+  return (
+    <List>
+      {Object.values(tasks).map((task) => (
+        <TaskRow
+          key={task.id}
+          before={<TaskCard taskId={task.id} />}
+          after={<ToggleTask taskId={task.id} />}
+        />
+      ))}
+    </List>
+  );
+}
+
+export default TaskList;
