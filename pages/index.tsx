e2095622e9352acebd2a3e90d1d96a8913e38e3c
Olga Ioffe <yoffic@yahoo.com>
2023-01-17 23:32:20 +0400

    add tasks to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
-import { Abhaya_Libre } from '@next/font/google';
+import { Abhaya_Libre, Actor } from '@next/font/google';
+import { Typography } from '@mui/material';
+
+import { TasksProvider } from '@app/providers/tasks';
+import MainLayout from 'views/main-layout';
+import TaskApp from 'views/tasks/task-app';
 
 const abhayaLibre = Abhaya_Libre({ weight: '600', subsets: ['latin'] });
+const actor = Actor({ weight: '400', subsets: ['latin'] });
 
 export default function Home() {
+  const [isSSR, setSSR] = useState<boolean>(true);
+
+  useEffect(() => {
+    setSSR(false);
+  }, []);
+
+  if (isSSR) {
+    return null;
+  }
+
   return (
     <>
       <Head>
@@ -11,7 +28,16 @@ export default function Home() {
         <meta name="description" content="Simple Todo App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={abhayaLibre.className}></main>
+      <main className={abhayaLibre.className}>
+        <MainLayout>
+          <div className={actor.className}>
+            <Typography variant="h1">To Do</Typography>
+          </div>
+          <TasksProvider>
+            <TaskApp />
+          </TasksProvider>
+        </MainLayout>
+      </main>
     </>
   );
 }
