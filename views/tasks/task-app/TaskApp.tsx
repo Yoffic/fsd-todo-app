98d470ff8057ccc2deac12cd3f96949b73b726bc
Olga Ioffe <yoffic@yahoo.com>
2023-01-17 23:31:38 +0400

    add task app

---
+++ views/tasks/task-app/TaskApp.tsx
@@ -0,0 +1,41 @@
+import { Stack } from '@mui/material';
+
+import { useTasksContext } from '@app/providers/tasks';
+import TaskList from 'views/tasks/task-list';
+import CurrentTasks from 'views/tasks/current-tasks';
+import TaskGroup from 'views/tasks/task-group';
+import {
+  getOtherTasks,
+  getTodayTasks,
+  getTomorrowTasks,
+  groupTasksByDate,
+} from '@entities/task/lib';
+import { Task } from '@entities/task/model';
+
+function TaskApp() {
+  const { tasks } = useTasksContext();
+
+  const taskListToRender = groupTasksByDate(tasks);
+  const todayTasks = getTodayTasks(taskListToRender);
+  const tomorrowTasks = getTomorrowTasks(taskListToRender);
+  const otherTasks = getOtherTasks(taskListToRender);
+  const otherDates = Object.keys(otherTasks);
+
+  return (
+    <Stack spacing="32px">
+      <CurrentTasks>
+        <TaskList tasks={todayTasks} />
+      </CurrentTasks>
+      <TaskGroup title="Tomorrow Tasks">
+        <TaskList tasks={tomorrowTasks} />
+      </TaskGroup>
+      {otherDates.map((date: Task['formattedDate']) => (
+        <TaskGroup title={`${date} Tasks`} key={date}>
+          <TaskList tasks={otherTasks[date]} />
+        </TaskGroup>
+      ))}
+    </Stack>
+  );
+}
+
+export default TaskApp;
